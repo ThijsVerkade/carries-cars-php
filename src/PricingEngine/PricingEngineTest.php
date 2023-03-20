c11c9e5a8ecb1330ff1f72e54d7ed53213d7f518
Robertbaelde <robert_baelde@hotmail.com>
2023-03-20 10:30:32 +0100

    Rename data provider

--- src/PricingEngine/PricingEngineTest.php
+++ src/PricingEngine/PricingEngineTest.php
@@ -10,7 +10,7 @@ class PricingEngineTest extends TestCase
 {
     /**
      * @test
-     * @dataProvider provideDurations
+     * @dataProvider provideDifferentDurationsAndPrices
      */
     public function calculate_price_charged_per_minute(Money $pricePerMinute, Duration $duration, Money $expectedPrice): void
     {
@@ -19,10 +19,10 @@ class PricingEngineTest extends TestCase
         $this->assertEquals($expectedPrice, $actual);
     }
 
-    public static function provideDurations(): iterable
+    public static function provideDifferentDurationsAndPrices(): iterable
     {
-        yield [Money::of(0.30, Currency::of('EUR')), Duration::ofMinutes(1), Money::of(0.30, Currency::of('EUR'))];
-        yield [Money::of(0.30, Currency::of('EUR')), Duration::ofMinutes(3), Money::of(0.90, Currency::of('EUR'))];
-        yield [Money::of(0.23, Currency::of('EUR')), Duration::ofMinutes(12), Money::of(2.76, Currency::of('EUR'))];
+        yield 'Duration of 1 minute with price of 0.30' => [Money::of(0.30, Currency::of('EUR')), Duration::ofMinutes(1), Money::of(0.30, Currency::of('EUR'))];
+        yield 'Duration of 3 minutes with price of 0.30' => [Money::of(0.30, Currency::of('EUR')), Duration::ofMinutes(3), Money::of(0.90, Currency::of('EUR'))];
+        yield 'Duration of 12 minutes with price of 0.23' => [Money::of(0.23, Currency::of('EUR')), Duration::ofMinutes(12), Money::of(2.76, Currency::of('EUR'))];
     }
 }
